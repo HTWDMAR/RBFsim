061240dfbb741162b70adf4e1d414706f1fd4312
Pratistha Kansakar <pratistha.k428@gmail.com>
2024-08-20 20:12:12 +0200

    Update home.py
    Add author

--- home.py
+++ home.py
@@ -89,9 +89,10 @@ Following authors are credited for this version [![DOI](https://zenodo.org/badge
 5. **M Gomez** - offline code and case studies
 6. Tassilo Reuschling - First offline code development
 7. Prof. Dr. T. Grischek - Supervisory support on contents
-8. Vibhu Batheja - GUI, Deployment and Verification
-9. Anton Kohler - Model Verification
-10. Pratistha Kansakar - Report creation and optimization
+8. Dr. Cornelius Sandhu
+9. Vibhu Batheja - GUI, Deployment and Verification
+10. Anton Kohler - Model Verification
+11. Pratistha Kansakar - Report creation and optimization
 
 
 
